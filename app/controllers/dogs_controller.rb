6b81d2a946c655329ff248f3ee1399405466c2de
Daniel Au <daniel.au@scouttalent.ca>
2018-12-09 21:55:52 -0800

    Use model

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,4 +6,9 @@ class DogsController < Rubidium::Controller
   def pomeranian
     render(:pomeranian, :personality => :big)
   end
+
+  def dog_1
+    dog_1 = Rubidium::Model::FileModel.find(1)
+    render :dog, dog: dog_1
+  end
 end
